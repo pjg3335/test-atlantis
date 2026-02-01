8f861b76dcaaac88ecfcb591a1280a844a0988a4
pjg3335 <jje3335@naver.com>
2026-02-01 16:58:50 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,10 @@
+# 모듈(≈함수)의 input
+variable "content_text" {
+    type = string
+}
+
+# 모듈(≈함수) 본체
+resource "local_file" "example" {
+  content  = var.content_text
+  filename = "${path.module}/result.txt"
+}
